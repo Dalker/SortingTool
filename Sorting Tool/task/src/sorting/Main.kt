ab6a4ea774b94e50d654967531e8a2d588f12fa7
Daniel Kessler (a.k.a. Dalker) <daniel.kessler@dalker.org>
2022-07-08 22:04:39 +0200

    Solved part 3, adding sorting handler base and numerical

--- Sorting Tool/task/src/sorting/Main.kt
+++ Sorting Tool/task/src/sorting/Main.kt
@@ -7,6 +7,7 @@ interface SortableDataType<T> {
     fun Scanner.getNext(): T
     fun getMax(data: List<T>): T?
     fun printMax(max: T)
+    fun sorted(data: List<T>): List<T>
 }
 
 class Sortable<T>(dataType: SortableDataType<T>) : SortableDataType<T> by dataType {
@@ -19,7 +20,12 @@ class Sortable<T>(dataType: SortableDataType<T>) : SortableDataType<T> by dataTy
         }
         data = mData.toList()
     }
-
+    fun reportSorted() {
+        val total = data.size
+        println("Total ${datumName}s: $total.")
+        print("Sorted data: ")
+        println(sorted(data).joinToString(" "))
+    }
     fun report() {
         val maxDatum = getMax(data)!!
         val total = data.size
@@ -34,29 +40,36 @@ class Sortable<T>(dataType: SortableDataType<T>) : SortableDataType<T> by dataTy
 class Numbers : SortableDataType<Long> {
     override val datumName = "number"
     override fun Scanner.getNext(): Long = this.nextLong()
+    override fun sorted(data: List<Long>) = data.sortedBy { it }
     override fun getMax(data: List<Long>) = data.maxOrNull()
-    override fun printMax(max: Long) = print("The greatest number: $max ")
+    override fun printMax(max: Long) = print("The greatest $datumName: $max ")
 }
 
 class Lines : SortableDataType<String> {
     override val datumName = "line"
     override fun Scanner.getNext(): String = this.nextLine()
+    override fun sorted(data: List<String>) = data.sortedBy { it }
     override fun getMax(data: List<String>) = data.maxByOrNull { it.length }
-    override fun printMax(max: String) = print("The longest line:\n$max\n")
+    override fun printMax(max: String) = print("The longest $datumName:\n$max\n")
 }
 
 class Words : SortableDataType<String> {
     override val datumName = "word"
     override fun Scanner.getNext(): String = this.next()
+    override fun sorted(data: List<String>) = data.sortedBy { it }
     override fun getMax(data: List<String>) = data.maxByOrNull { it.length }
-    override fun printMax(max: String) = print("The longest word: $max ")
+    override fun printMax(max: String) = print("The longest $datumName: $max ")
 }
 
 fun main(args: Array<String>) {
-    val dataType = if (args.size > 1 && args[0] == "-dataType") args[1] else "word"
-    when(dataType) {
-        "long" -> Sortable(Numbers())
-        "line" -> Sortable(Lines())
-        else -> Sortable(Words())
-    }.report()
+    if (args.contains("-sortIntegers")) {
+       Sortable(Numbers()).reportSorted()
+    } else {
+        val dataType = if (args.size > 1 && args[0] == "-dataType") args[1] else "word"
+        when(dataType) {
+            "long" -> Sortable(Numbers())
+            "line" -> Sortable(Lines())
+            else -> Sortable(Words())
+        }.report()
+    }
 }
